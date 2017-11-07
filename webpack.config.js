e7d1993a30f8f9e52c62d0e72cb111f243121c88
Alex Hunt <alexhunt@Alexs-MacBook-Pro.local>
2017-11-06 21:58:31 -0500

    Basic layout styling and component tweaks. Moved Bootstrap dependencies behind Webpack and CSS into Sass.

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,22 @@ const path = require('path');
 const rootAssetPath = './src/';
 
 let pathsToClean = [
-  'static/js/'
+  'static/'
 ]
 
 module.exports = {
 	entry: {
 		reader: [
-			rootAssetPath + 'reader'
-		]
+			rootAssetPath + 'reader',
+			rootAssetPath + 'stylesheets/' + 'reader.scss'
+		],
+		// style: [
+		// ]
 	},
 	output: {
 		publicPath: "static/js/",
     	filename: 'reader.[chunkhash].js',
-    	path: path.resolve(__dirname, 'static/js')
+    	path: path.resolve(__dirname, 'static/js/')
 	},
 	watch: true,
 	watchOptions: {
@@ -27,20 +30,46 @@ module.exports = {
 		ignored: /node_modules/
 	},
 	module : {
-		rules: [{
+		rules: [
+		{
+			test: /\.(js)$/,
 	    	use: {
 				loader: 'babel-loader',
 				options: {
 					presets: ['env', 'react']
 	        	}
 			}
-	    }]
-	},
+		},
+		{
+		    test: /\.(scss)$/,
+		    use: [{
+		    	loader: 'style-loader', // inject CSS to page
+		    }, {
+		    	loader: 'css-loader', // translates CSS into CommonJS modules
+		    }, {
+		    	loader: 'postcss-loader', // Run post css actions
+		    	options: {
+		        	plugins: [
+		            	require('precss'),
+		            	require('autoprefixer')
+		          	]
+		        }
+		    }, {
+		      loader: 'sass-loader' // compiles SASS to CSS
+		    }]
+	    }
+    ]},
 	plugins: [
 		// TODO: Dev / Prod Config
 		// new webpack.optimize.UglifyJsPlugin(),
         new ManifestRevisionPlugin(path.join('static/js', 'manifest.json'), {
             rootAssetPath: rootAssetPath
+        }),
+        new webpack.ProvidePlugin({
+	        $: 'jquery',
+	        jQuery: 'jquery',
+	        'window.jQuery': 'jquery',
+	        Popper: ['popper.js', 'default']        	
         }),
         new CleanWebpackPlugin(pathsToClean)
 	],	

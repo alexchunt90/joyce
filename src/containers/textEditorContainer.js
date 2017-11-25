1aa4fcf1f6a9b4c98b0a4aa20d2f14d7e296a31d
Alex Hunt <alex.c.hunt90@gmail.com>
2017-11-25 09:57:25 -0500

    Used DraftJS RichUtil library to wire up bold/italic/underline/header buttons.

--- src/containers/textEditorContainer.js
+++ src/containers/textEditorContainer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Editor } from 'draft-js'
+import { Editor, RichUtils } from 'draft-js'
 import { stateToHTML } from 'draft-js-export-html'
 
 import { updateEditorState, updateChapterTitleInput, updateChapterNumberInput, submitChapter, deleteChapter } from '../actions'
 
 
-const TextEditor = ({currentChapter, editorState, onChangeEditorState, chapterTitleInput, onChapterTitleChange, onEditSubmit, onDelete}) =>
+const TextEditor = ({currentChapter, editorState, handleKeyCommand, onChangeEditorState, onBoldClick, onItalicClick, onUnderlineClick, onHeaderClick, chapterTitleInput, onChapterTitleChange, onEditSubmit, onDelete}) =>
 	<div id='editor_container'>
 		<div id='editor_topbar'>
 			<div className='row'>			
@@ -19,21 +19,33 @@ const TextEditor = ({currentChapter, editorState, onChangeEditorState, chapterTi
 
 			</div>
 			<div className='row'>
-				<div className='btn-group col-md-3 offset-md-6' role='group'>
-					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-bold'></i></button>
-					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-italic'></i></button>
-					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-underline'></i></button>
+				<div className='btn-group col-md-3' role='group'>
+					<button type='button' onClick={()=>onBoldClick(editorState)} className='btn btn-info btn-sm'><i className='fa fa-bold'></i></button>
+					<button type='button' onClick={()=>onItalicClick(editorState)} className='btn btn-info btn-sm'><i className='fa fa-italic'></i></button>
+					<button type='button' onClick={()=>onUnderlineClick(editorState)} className='btn btn-info btn-sm'><i className='fa fa-underline'></i></button>
+					<button type='button' onClick={()=>onHeaderClick(editorState)} className='btn btn-info btn-sm'><i className='fa fa-header'></i></button>
 				</div>
 				<div className='btn-group col-md-3' role='group'>
 					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-align-left'></i></button>
 					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-align-center'></i></button>
 					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-align-right'></i></button>			
 				</div>
+				<div className='btn-group col-md-1' role='group'>
+					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-file-image-o'></i></button>
+				</div>
+				<div className='btn-group col-md-2' role='group'>
+					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-indent'></i></button>
+					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-outdent'></i></button>
+				</div>								
+				<div className='btn-group col-md-2' role='group'>
+					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-undo'></i></button>
+					<button type='button' className='btn btn-info btn-sm'><i className='fa fa-repeat'></i></button>
+				</div>
 			</div>
 		</div>
 
 		<div id='editor_content'>
-			<Editor editorState={editorState} onChange={onChangeEditorState} />
+			<Editor editorState={editorState} handleKeyCommand={handleKeyCommand} onChange={onChangeEditorState} />
 		</div>
 
 		<div id='editor_bottombar'>
@@ -74,6 +86,26 @@ const mapDispatchToProps = dispatch => {
 			if (currentChapter.title !== '') {
 				dispatch(deleteChapter(currentChapter.number))
 			}
+		},
+		handleKeyCommand: (command, editorState) => {
+			const newState = RichUtils.handleKeyCommand(editorState, command)
+			dispatch(updateEditorState(newState))
+		},
+		onBoldClick: (editorState) => {
+			const newState = RichUtils.toggleInlineStyle(editorState, 'BOLD')
+			dispatch(updateEditorState(newState))
+		},
+		onItalicClick: (editorState) => {
+			const newState = RichUtils.toggleInlineStyle(editorState, 'ITALIC')
+			dispatch(updateEditorState(newState))
+		},
+		onUnderlineClick: (editorState) => {
+			const newState = RichUtils.toggleInlineStyle(editorState, 'UNDERLINE')
+			dispatch(updateEditorState(newState))
+		},
+		onHeaderClick: (editorState) => {
+			const newState = RichUtils.toggleBlockType(editorState, 'header-two')
+			dispatch(updateEditorState(newState))
 		},
 		onEditSubmit: (currentChapter, chapterTitleInput, editorState) => {
 			let textContent = editorState.getCurrentContent()

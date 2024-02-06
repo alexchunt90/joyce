253b9d70e466d2c88576539d0dbe97cb36798038
alexchunt90 <alex.c.hunt90@gmail.com>
2024-02-05 23:48:38 -0800

    Certbot changes

--- setup/.stage.sh
+++ setup/.stage.sh
@@ -1,3 +1,6 @@
 npm run stage
 docker compose build
 docker compose up -d
+
+#docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d joyce-staging.net
+#docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d joyce-staging.net

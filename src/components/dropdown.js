37fbe728371624dfb7fe91b6d5b0f98f00ea5910
alexchunt90 <alex.c.hunt90@gmail.com>
2024-03-05 11:11:55 -0800

    Add filter to mobile topbar

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import helpers from '../modules/helpers'
 import TagColorPreview from './tagColorPreview'
+import FilterInput from './filterInput'
 
 export const DocTypeDropdown = ({docType, setDocType, size='sm'}) =>
 	<div className='dropdown'>
@@ -19,29 +21,47 @@ export const DocTypeDropdown = ({docType, setDocType, size='sm'}) =>
 		</div>
 	</div>
 
-export const DocListDropdown = ({currentDocument, docs, docType, onDocumentClick, editMode=false, onNewDocumentClick, theme='primary'}) =>
-	<div className='dropdown'>
-		<button className={'btn btn-' + theme +' btn-md dropdown-toggle'} type='button' id='doc_type_select' data-bs-toggle='dropdown'>
-			{docType==='chapters' && currentDocument.number ? currentDocument.number + '. ' : ''}{currentDocument.title}
-		</button>
-		<div id='doc_list_dropdown' className='dropdown-menu'>
-	    	{docs.map(doc =>
-				<a key={doc.id} className='dropdown-item' onClick={()=>onDocumentClick(doc.id, docType)}>
-					{docType==='tags' &&
-						<TagColorPreview color={doc.color}/>
-					}
-					{docType==='chapters' ? doc.number + '. ' : ''}
-					{doc.title}
-				</a>
-	    	)}
-	    	{editMode===true &&
-	    		<a className='dropdown-item' onClick={onNewDocumentClick}>
-					<i className='fas fa-plus-square'></i>&nbsp;
-					New {helpers.docTypeName(docType)}
-	    		</a>
-	    	}
+export const DocListDropdown = ({currentDocument, docs, docType, onDocumentClick, editMode=false, onNewDocumentClick, theme='primary'}) => {
+	const filterInputText = useSelector((state) => state.inputs.filterInput).toLowerCase()
+	let filteredDocArray = docs
+
+	if (filterInputText !== '') {			
+		filteredDocArray = filteredDocArray.filter((doc) => {
+			if (typeof doc.title === 'string') {
+				const docTitle = doc.title.toLowerCase()
+				return docTitle.includes(filterInputText)
+			} else {return false}
+		})
+	}		
+
+	return (
+		<div className='dropdown'>
+			<button className={'btn btn-' + theme +' btn-md dropdown-toggle'} type='button' id='doc_type_select' data-bs-toggle='dropdown'>
+				{currentDocument.title}
+			</button>
+			<div id='doc_list_dropdown' className='dropdown-menu'>
+				{docType === 'notes' &&
+					<FilterInput />
+				}
+		    	{filteredDocArray.map(doc =>
+					<a key={doc.id} className='dropdown-item' onClick={()=>onDocumentClick(doc.id, docType)}>
+						{docType==='tags' &&
+							<TagColorPreview color={doc.color}/>
+						}
+						{docType==='chapters' ? doc.number + '. ' : ''}
+						{doc.title}
+					</a>
+		    	)}
+		    	{editMode===true &&
+		    		<a className='dropdown-item' onClick={onNewDocumentClick}>
+						<i className='fas fa-plus-square'></i>&nbsp;
+						New {helpers.docTypeName(docType)}
+		    		</a>
+		    	}
+			</div>
 		</div>
-	</div>
+	)
+}
 
 DocTypeDropdown.propTypes = {
 	docType: PropTypes.string,

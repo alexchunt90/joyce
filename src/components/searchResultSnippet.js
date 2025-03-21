1f3721e87ba9cb2da3227bb3e850b2268729609b
Alexander Hunt <alexchunt@Alexanders-MacBook-Pro.local>
2025-03-21 10:58:07 -0700

    Fix page breaks showing up in search results

--- src/components/searchResultSnippet.js
+++ src/components/searchResultSnippet.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-
+const stripPageBreaksFromSearchText = (snippetText) => {
+	const pageBreakPattern = /[0-9]{4,4}\#[0-9]{1,3}/
+	const strippedText = snippetText.replace(pageBreakPattern, '')
+	return strippedText
+}
 
 const buildSnippetLink = (key, docType, route) => {
 	if (docType === 'chapter') {
@@ -17,7 +21,7 @@ const buildSnippetLink = (key, docType, route) => {
 const SearchResultSnippet = ({snippet, docType, route, onClick}) =>
 	<div id={snippet.key}>
 		<Link to={buildSnippetLink(snippet.key, docType, route)} >
-			<div dangerouslySetInnerHTML={{__html: snippet.text}}>
+			<div dangerouslySetInnerHTML={{__html: stripPageBreaksFromSearchText(snippet.text)}}>
 			</div>
 			{/*{snippet.text}*/}
 		</Link>

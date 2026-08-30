40f072a78d184693f94591a53fe61cff756f8156
Alexander Hunt <alexchunt@Alexanders-MacBook-Pro.local>
2026-08-29 21:13:13 -0700

    Matching nginx.conf and app.py to production server

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ config_params = {
 application = Flask(__name__)
 application.config.update(config_params)
 jwt = JWTManager(application)
-CORS(application, origins=['https://localhost', 'https://joyce-staging.net', 'https://joyceproject.com', 'https://www.joyceproject.com'])
+CORS(application, supports_credentials=True, origins=['https://localhost', 'https://joyce-staging.net', 'https://joyceproject.com', 'https://www.joyceproject.com', 'https://m.joyceproject.com'])
 
 # Register blueprints
 application.register_blueprint(joyce)
@@ -42,5 +42,5 @@ application.register_blueprint(search_api, url_prefix='/api/search')
 application.register_blueprint(google_auth_api, url_prefix='/auth')
 
 if __name__ == "__main__":
-	application.run(debug=False, threaded=True, host="0.0.0.0", port=443, ssl_context=('server.crt', 'server.key'))
-    # application.run(debug=False, threaded=True, host="0.0.0.0", port=8080)
+	# application.run(debug=False, threaded=True, host="0.0.0.0", port=443, ssl_context=('server.crt', 'server.key'))
+	application.run(debug=False, threaded=True, host="0.0.0.0", port=8080)
